5b31ddaec335a684d46892d7683f88f70ad788a5
zx <970389745@qq.com>
2018-05-10 20:02:02 +0800

    Initial commit

---
+++ src/main/java/com/zx/annoations/ThreadSafe.java
@@ -0,0 +1,21 @@
+package com.zx.annoations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * author:ZhengXing
+ * datetime:2018-05-10 19:56
+ * 表明线程安全的注解
+ *
+ * 注解在类上
+ * 只存在源码中，编译时忽略
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface ThreadSafe {
+
+    String value() default "";
+}
